7a3d8ec6a13dd9ec621cf0b3cc36a59837cf32a6
olegz <ozenderm@lakeahu.ca>
2024-01-16 19:21:43 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   scripts/variables.js

--- scripts/variables.js
+++ scripts/variables.js
@@ -8,12 +8,17 @@
     console.log(myName + " - "+ typeof(myAge));
     myAge = 21;
     console.log(myAge + " - "+ typeof(myName));
+
     /* STEP 2: Initialize variables
 	Don't confuse a variable with no value with one that doesn't exist at all */
-
+    myAge = 80;
+    myAge = "eighty";
     /* STEP 3: Updating variables
 	Once a variable has been declared, this can be done any time */
-
+    var num1 = 20;
+    console.log(num1 + " - "+ typeof(num1));
+    var num2 = 19.999999999999;
+    console.log(num2 + " - "+ typeof(num2));
     // STEP 4: Variable types
     // Numbers
 

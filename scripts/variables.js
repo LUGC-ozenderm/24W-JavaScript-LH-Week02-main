90c5929785c5483f371747ee9048cc56c6b69726
olegz <ozenderm@lakeahu.ca>
2024-01-16 19:31:18 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   scripts/variables.js

--- scripts/variables.js
+++ scripts/variables.js
@@ -16,6 +16,7 @@
     /* STEP 3: Updating variables
 	Once a variable has been declared, this can be done any time */
     myAge = 21;
+    myAge = myAge + 1;
     // STEP 4: Variable types
     // Numbers
     var num1 = 20;
@@ -23,7 +24,10 @@
     var num2 = 19.999999999999;
     console.log(num2 + " - "+ typeof(num2));
     // Strings (use '' or "")
-
+    var string1 = "Hello";
+    console.log(string1 + " - "+ typeof(string1));
+    var string2 = 'World';
+    console.log(string2 + " - "+ typeof(string2));
     // Boolean
     var bool1 = true;
     console.log(bool1 + " - "+ typeof(bool1));
@@ -32,8 +36,8 @@
     // Arrays
     var array1 = [1,2,3];
     console.log(array1 + " - "+ typeof(array1));
-    var array2 = ["one","two","three"];
-    console.log(array2 + " - "+ typeof(array2));
+    var mixedArray = [1, "two", true];
+    console.log(mixedArray + " - "+ typeof(mixedArray));
     // Objects
     var object1 = {name: "John", age: 21};
     console.log(object1 + " - "+ typeof(object1));
